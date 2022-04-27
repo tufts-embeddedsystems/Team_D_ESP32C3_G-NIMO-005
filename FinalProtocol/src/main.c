6854e5bc434f9cfd321b3251407ab0fb0c5c1fe5
theTackyW <43731631+theTackyW@users.noreply.github.com>
2022-04-27 16:13:51 -0400

    Update main.c
    Added adc readings

--- FinalProtocol/src/main.c
+++ FinalProtocol/src/main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
@@ -22,19 +23,20 @@ struct packet {
     uint8_t team_data;
 };
 
-#define const uint64_t TIME_ASLEEP 3600000000
+#define TIME_ASLEEP 3600000000
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
 
 static esp_adc_cal_characteristics_t *adc_chars;
-#if CONFIG_IDF_TARGET_ESP32
-static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+//#if CONFIG_IDF_TARGET_ESP32
+static const adc_channel_t channel_sensor = ADC_CHANNEL_1;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_channel_t channel_therm = ADC_CHANNEL_3;     //GPIO34 if ADC1, GPIO14 if ADC2
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-#elif CONFIG_IDF_TARGET_ESP32S2
-static const adc_channel_t channel = ADC_CHANNEL_6;     // GPIO7 if ADC1, GPIO17 if ADC2
-static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
-#endif
+//#elif CONFIG_IDF_TARGET_ESP32S2
+//static const adc_channel_t channel = ADC_CHANNEL_6;     // GPIO7 if ADC1, GPIO17 if ADC2
+//static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
+//#endif
 static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1;
 
@@ -81,21 +83,49 @@ void app_main() {
     
     struct packet packets[10]; // Array of packets to store data 
 
-    esp_err_t esp_sleep_enable_timer_wakeup(TIME_ASLEEP); // Enable sleep mode timer
+    //Configure ADC
+    adc1_config_width(width);
+    adc1_config_channel_atten(channel_sensor, atten);
+    adc1_config_channel_atten(channel_therm, atten);
+    esp_err_t esp_sleep_enable_timer_wakeup((uint64_t) TIME_ASLEEP); // Enable sleep mode timer
+
+    int32_t sensor_reading=0;
+    int32_t thermistor_reading=0;
 
     uint8_t count = 0; // Amount of packets stored in current batch
-    while(1) {
-        int32_t sensor_reading;
-        int32_t thermistor_reading;
-        uint8_t battery;
-        
-        count++;
+
+    while (1) {
+
+    //Multisampling
+    for (int i = 0; i < NO_OF_SAMPLES; i++) {
+        sensor_reading += adc1_get_raw((adc1_channel_t)channel_sensor);
+        thermistor_reading += adc1_get_raw((adc1_channel_t)channel_therm);
+    }
+    sensor_reading /= NO_OF_SAMPLES;
+    thermistor_reading /= NO_OF_SAMPLES;
+
+    //Convert adc_reading to voltage in mV
+    uint32_t voltage_sensor = esp_adc_cal_raw_to_voltage(sensor_reading, adc_chars);
+    uint32_t voltage_therm = esp_adc_cal_raw_to_voltage(thermistor_reading, adc_chars);
+    int32_t temp_sensor = Convert2Temp(voltage_sensor);
+    int32_t temp_therm = Convert2Temp(voltage_therm);
+
+    packets[count].sensor_temp = temp_sensor;
+    packets[count].thermistor_temp = temp_therm;
+    //packets[count].time
+    packets[count].battery = 255;
+    packets[count].data_len = 0;
+
+    count++;
         if (count == 10) {
             // DO MQTT
 
             count = 0;
         }
-
         esp_deep_sleep_start(); // Enter deep sleep
     }
+}
+
+int32_t Convert2Temp(uint32_t volt){
+    return ((volt/3300)*175.72-46.85)*1000;
 }
